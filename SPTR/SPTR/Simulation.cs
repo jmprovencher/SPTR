0c8588e55031103052ab2e3eb1146117a0384f89
Rem0o <rem54321@hotmail.com>
2016-04-20 14:59:30 -0400

    enlever un truc inutile en tbk

--- SPTR/SPTR/Simulation.cs
+++ SPTR/SPTR/Simulation.cs
@@ -152,14 +152,10 @@ namespace SPTR
 
         public void runParcours(int temps)
         {
-            int count = 0;
+
             foreach (Parcours parcours in ListeParcours)
             {
-                count++;
-                if (count != 1)
-                {
-                    continue;
-                }
+
                 //mouvement des voitures
                 string ParcoursDirection = parcours.getDirection();
                 Voiture voitureTerminee = null;
